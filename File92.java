a0489e145157357acba4bc8fe238280ac104e9c7
Naman Rawat <namanrawat90.nr@gmail.com>
2026-02-01 15:36:42 +0530

    Consumer, Supplier

--- File92.java
+++ File92.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class File92 {
@@ -8,7 +10,7 @@ public class File92 {
   public static void main(String[] args) {
 
     Consumer<String> consumer = s -> System.out.println(s);
-    // consumer.accept("naman");
+    consumer.accept("naman");
 
     Consumer<List<Integer>> listConsumer1 = li -> {
       for (Integer i : li) {
@@ -30,6 +32,20 @@ public class File92 {
 
     Supplier<Integer> supplier = () -> 1;
     System.out.println(supplier.get());
+
+    Supplier<String> giveHelloWorld = () -> "Hello World";
+    System.out.println(giveHelloWorld.get());
+
+    // Combined Example
+
+    Predicate<Integer> pred = x -> x % 2 == 0;
+    Function<Integer, Integer> func = x -> x * 2;
+    Consumer<Integer> consumer1 = x -> System.out.println(x);
+    Supplier<Integer> supplier1 = () -> 100;
+
+    if (pred.test(supplier1.get())) {
+      consumer1.accept(func.apply(supplier1.get()));
+    }
 
   }
 
