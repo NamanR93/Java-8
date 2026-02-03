cdedc1d45cc5070ef34d2923bb1153e8aad7f88e
Naman Rawat <namanrawat90.nr@gmail.com>
2026-02-03 19:52:18 +0530

    Bi

---
+++ File94.java
@@ -0,0 +1,18 @@
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+
+public class File94 {
+  public static void main(String[] args) {
+
+    UnaryOperator<Integer> unaryOp = x -> x * x;
+    System.out.println(unaryOp.apply(5));
+
+    BiFunction<String, String, String> biFunc = (str1, str2) -> str1 + str2;
+
+    BinaryOperator<String> binary = (str1, str2) -> str1 + str2;
+    System.out.println(binary.apply("hi", "hello"));
+
+  }
+
+}
